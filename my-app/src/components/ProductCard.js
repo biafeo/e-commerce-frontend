ddd04af9a54e340427a7f79defc023eec5c239bf
beatriz <beatrizffeo@gmail.com>
2024-04-12 14:29:06 -0600

    products displaying in the right sizes

--- my-app/src/components/ProductCard.js
+++ my-app/src/components/ProductCard.js
@@ -1,16 +1,27 @@
 import React, { useState } from "react";
 
 function ProductCard({ product }) {
-  const { title, price, category, description, image } = product;
+  const { title, price, description, image } = product;
+  const [showDescription, setShowDescription] = useState(false);
+
+  const toggleDescription = () => {
+    setShowDescription(!showDescription);
+  };
 
   return (
     <li className="card">
       <h1>{title}</h1>
       <img src={image} alt={title} />
-      <p>Price:{price}</p>
-      <p>Category:{category}</p>
-      <p>Description: {description}</p>
+      <h1>Price: ${price}</h1>
+      {showDescription && <p>Description: {description}</p>}
+      <div className="button-container">
+        <button className="button-see-more" onClick={toggleDescription}>
+          {showDescription ? "See less" : "See more"}
+        </button>
+        <button className="button-add-to-cart">Add to Cart</button>
+      </div>
     </li>
   );
 }
+
 export default ProductCard;
