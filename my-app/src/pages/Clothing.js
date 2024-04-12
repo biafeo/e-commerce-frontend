7d68a61e8888f184d7c5870ae5a40a06e78276ba
Hollis Crowder <hollis.crowder@gmail.com>
2024-04-12 11:29:12 -0700

    create NavBar feature & fix header height

---
+++ my-app/src/pages/Clothing.js
@@ -0,0 +1,14 @@
+import NavBar from "../components/NavBar";
+
+function Clothing() {
+  return (
+    <>
+      <header>
+        <h1>Clothing</h1>
+      </header>
+      <main></main>
+    </>
+  );
+}
+
+export default Clothing;
