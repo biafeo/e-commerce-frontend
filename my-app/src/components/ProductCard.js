6909474ba2a07347a62fdbb689315b781d837171
Hollis Crowder <hollis.crowder@gmail.com>
2024-04-16 11:51:34 -0700

    fix product card

--- my-app/src/components/ProductCard.js
+++ my-app/src/components/ProductCard.js
@@ -2,33 +2,34 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import SeeMoreCard from "./SeeMoreCard";
 import "./ProductCard.css";
-
 function ProductCard({ product, addToCart }) {
   const { id, title, price, image } = product;
   const [showMore, setShowMore] = useState(false);
-
   const toggleShowMore = () => {
     setShowMore(!showMore);
   };
-
   return (
     <li className="card">
       <h1>{title}</h1>
       <img src={image} alt={title} />
       <h1>Price: ${price}</h1>
       <div className="button-container">
-        <button className="button-see-more" onClick={toggleDescription}>
-          {showDescription ? "See less" : "See more"}
-        </button>
-        <button
-          onClick={() => addToCart(product)}
-          className="button-add-to-cart"
-        >
+        {showMore ? (
+          <SeeMoreCard product={product} removeFromCart={toggleShowMore} />
+        ) : (
+          <Link
+            to={`/product/${id}`}
+            className="button-see-more"
+            onClick={toggleShowMore}
+          >
+            See more
+          </Link>
+        )}
+        <button onClick={addToCart} className="button-add-to-cart">
           Add to Cart 🛒
         </button>
       </div>
     </li>
   );
 }
-
 export default ProductCard;
